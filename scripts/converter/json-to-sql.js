bc7de064a357a9d89469205aef7931c8a7a5f75b
Lucas Josino <contact@lucasjosino.com>
2023-06-24 11:40:11 -0300

    refactor: Refactor code and Add docs

--- scripts/converter/json-to-sql.js
+++ scripts/converter/json-to-sql.js
@@ -2,17 +2,17 @@ import fs from "fs/promises";
 
 import { DB_FOLDER, TRANSLATION_TABLES, VERSION } from "../constants.js";
 import { getSingular } from "../utils.js";
-import { debug, log } from "console";
+import { log } from "console";
 
 const SPECIAL_NAMES = ["character", "language"];
-const OUTPUT_PATH = `./${DB_FOLDER}/${VERSION}/data/sql`;
+const BASE_PATH = `./${DB_FOLDER}/${VERSION}/data`;
+const OUTPUT_PATH = `${BASE_PATH}/sql`;
 
 /**
  * Main script to convert json files into sqls
  */
 async function main() {
-  const path = `./${DB_FOLDER}/${VERSION}/data`;
-  const files = await fs.readdir(path, {
+  const files = await fs.readdir(BASE_PATH, {
     recursive: false,
   });
 
@@ -22,24 +22,18 @@ async function main() {
   await fs.writeFile(`${OUTPUT_PATH}/hawapi.sql`, "");
 
   let queries = [];
-  for (const index in files) {
-    let target = files[index];
-    if (!(await fs.stat(`${path}/${target}`)).isDirectory()) {
+  for (let target of files) {
+    if (!(await fs.stat(`${BASE_PATH}/${target}`)).isDirectory()) {
       // Remove file extension
       target = target.substring(0, target.lastIndexOf("."));
 
-      let data = await fs.readFile(
-        `./${DB_FOLDER}/${VERSION}/data/${target}.json`
-      );
-      data = JSON.parse(data);
+      let data = await getJsonFile(target, false);
 
-      for (const item in data) {
-        const uuid = data[item]["uuid"];
-
-        let { fields, values, subQueries } = query(target, data[item]);
+      for (const item of data) {
+        let { fields, values, subQueries } = query(target, item);
 
         if (TRANSLATION_TABLES.includes(target)) {
-          let res = await translationQuery(target, uuid);
+          let res = await translationQuery(target, item.uuid);
           subQueries.push(...res);
         }
 
@@ -51,24 +45,27 @@ async function main() {
     }
   }
 
-  debug(process.memoryUsage());
   await fs.writeFile(`${OUTPUT_PATH}/hawapi.sql`, queries);
 
   log(
-    `Converted '${path}' files into SQL (Output: '${OUTPUT_PATH}/hawapi.sql')`
+    `Converted '${BASE_PATH}' files into SQL (Output: '${OUTPUT_PATH}/hawapi.sql')`
   );
 }
 
+/**
+ * Method that query all target translation
+ * @param {string} target The target (actors, episodes...) of the request
+ * @param {string} uuid The item identification
+ * @returns An SQL INSERT string with target translation
+ * @since 1.0.0
+ */
 const translationQuery = async (target, uuid) => {
-  let data = await fs.readFile(
-    `./${DB_FOLDER}/${VERSION}/data/translations/${target}_translations.json`
-  );
-  data = JSON.parse(data);
+  let data = await getJsonFile(target, true);
 
   let res = [];
-  for (const item in data) {
-    if (data[item][`${getSingular(target)}_uuid`] === uuid) {
-      let { fields, values } = query(target, data[item]);
+  for (const item of data) {
+    if (item[`${getSingular(target)}_uuid`] === uuid) {
+      let { fields, values } = query(target, item);
 
       let sqlQuery =
         `INSERT INTO ${target}_translations (` +
@@ -84,16 +81,25 @@ const translationQuery = async (target, uuid) => {
   return res;
 };
 
-const subQuery = (target, fieldName, array) => {
+/**
+ * Method that 'query' a field from main query
+ * @param {string} target The target (actors, episodes...) of the request
+ * @param {string} field The field name representing the 'sub query'
+ * @param {string[]} array All values from field of main query
+ * @returns An SQL INSERT string
+ * @since 1.0.0
+ */
+const subQuery = (target, field, array) => {
   let res = "";
-  for (const item in array) {
-    // Ignore any possible array or non dictionary/map
-    if (typeof array[item] !== "object" || Array.isArray(item)) return "";
 
-    let { fields, values } = query(target, array[item]);
+  for (const item of array) {
+    // Ignore any possible array or non dictionary/map
+    if (typeof item !== "object" || Array.isArray(item)) return "";
+
+    let { fields, values } = query(target, item);
 
     res +=
-      `INSERT INTO ${target}_${fieldName} (` +
+      `INSERT INTO ${target}_${field} (` +
       fields +
       ") VALUES (" +
       values +
@@ -103,6 +109,13 @@ const subQuery = (target, fieldName, array) => {
   return res;
 };
 
+/**
+ * Method that query and create a SQL INSERT string
+ * @param {string} target The target (actors, episodes...) of the request
+ * @param {string[]} array All values from file/field
+ * @returns All fields, values and subQueries from file/field
+ * @since 1.0.0
+ */
 const query = (target, array) => {
   let subQueries = [];
   let fields = "";
@@ -149,15 +162,51 @@ const query = (target, array) => {
   };
 };
 
+/**
+ * Method that converts all items inside the array into a PostgreSQL array
+ * @param {string[]} array All values from field
+ * @returns An string representing the PostgreSQL array
+ * @since 1.0.0
+ */
 const objectToPostgreSQLArray = (array) => {
   let res = "{";
-  for (const value in array) {
-    res += `${array[value]}, `;
+
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+
+    if (typeof value === "string") {
+      res += `"${value}", `;
+      continue;
+    }
+
+    res += `${value}, `;
   }
-  res = res.slice(0, -2);
-  return res + "}";
+
+  return res.slice(0, -2) + "}";
 };
 
+/**
+ * Method that read and parse the json file using default path
+ * @param {string} target The target (actors, episodes...) of the request
+ * @param {boolean} isTranslation Define target is from a normal or translation file
+ * @returns An JSON with all file items
+ * @since 1.0.0
+ */
+const getJsonFile = async (target, isTranslation) => {
+  const res = await fs.readFile(
+    isTranslation
+      ? `${BASE_PATH}/translations/${target}_translations.json`
+      : `${BASE_PATH}/${target}.json`
+  );
+
+  return JSON.parse(res);
+};
+
+/**
+ * Method that escape/fix single quotes from string (according to SQL)
+ * @param {string} value The string to be fixed
+ * @returns An quote fixed string
+ */
 const escapeSingleQuote = (value) => {
   return value.replace(/'/g, "''");
 };
