72683a5fa255c756c017d30c7b9595dadf264662
Lucas Josino <contact@lucasjosino.com>
2024-06-15 21:21:56 -0300

    refactor: delete js scripts

--- scripts/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Method to get singular name of table
- * @param {string} value The table name
- * @returns Singular version of table name
- */
-export const getSingular = (value) => {
-  return value.slice(0, -1);
-};
